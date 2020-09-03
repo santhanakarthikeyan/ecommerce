6b9be3a6bc3f7c86bce5f5fac529c2396a0e6dc0
santhanakarthikeyan <santhanakarthikeyan.s@gmail.com>
2020-09-03 15:55:26 +0530

    Added db/seed.rb file to populate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Promotion.create!([
+  {id: 2, name: "MultiplesOfThree", qty: 3, basket_price: nil, discount_price: 15.0, type: "ProductPromotion"},
+  {id: 3, name: "MultiplesOfTwo", qty: 2, basket_price: nil, discount_price: 5.0, type: "ProductPromotion"},
+  {id: 1, name: "BasketPromotion", qty: nil, basket_price: 150.0, discount_price: 20.0, type: "BasketPromotion"}
+])
+Product.create!([
+  {name: "A", price: 30.0, promotion_id: 2},
+  {name: "B", price: 20.0, promotion_id: 3},
+  {name: "C", price: 50.0, promotion_id: nil},
+  {name: "D", price: 15.0, promotion_id: nil}
+])
+#Promotion.create!([
+  #{name: "BasketPromotion", qty: nil, basket_price: 150.0, discount_price: 20.0, type: "BasketPromotion"}
+#])
+#Promotion.create!([
+  #{name: "MultiplesOfThree", qty: 3, basket_price: nil, discount_price: 15.0, type: "ProductPromotion"},
+  #{name: "MultiplesOfTwo", qty: 2, basket_price: nil, discount_price: 5.0, type: "ProductPromotion"}
+#])
