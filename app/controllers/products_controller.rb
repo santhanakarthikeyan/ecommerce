f313f15cb3f25a3e44d4d88abe486d25eaae2cbd
santhanakarthikeyan <santhanakarthikeyan.s@gmail.com>
2020-09-03 11:11:21 +0530

    Added clear cart feature to empty cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,11 @@ class ProductsController < ApplicationController
     end
   end
 
+  def clear_cart
+    session[:item_list] = []
+    redirect_to products_url, notice: 'Cart is empty!'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :products do
     post :add_to_cart
   end
+
+  delete :clear_cart, controller: 'products'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
